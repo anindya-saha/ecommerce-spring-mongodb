fd58ac68fa4b3fcf25ee92e1de19d12b2236dd32
İlker Korkut <ilkerkorkut@outlook.com>
2013-09-11 01:11:23 +0300

    category tree

--- src/main/java/com/ecommerce/dao/CategoryDaoImpl.java
+++ src/main/java/com/ecommerce/dao/CategoryDaoImpl.java
@@ -1,13 +1,15 @@
 package com.ecommerce.dao;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.mongodb.core.MongoTemplate;
+import org.springframework.data.mongodb.core.query.Criteria;
+import org.springframework.data.mongodb.core.query.Query;
 import org.springframework.stereotype.Repository;
 
 import com.ecommerce.model.Category;
-import com.ecommerce.model.Product;
 
 @Repository
 public class CategoryDaoImpl implements CategoryDao {
@@ -17,6 +19,7 @@ public class CategoryDaoImpl implements CategoryDao {
 	
 	public static final String COLLECTION_NAME = "category";
 	
+	//Get All Categories
 	@Override
 	public List<Category> getAllCategory() {
 		
@@ -24,12 +27,42 @@ public class CategoryDaoImpl implements CategoryDao {
 		
 	}
 	
+	//Get Single Category with its id
 	@Override
 	public Category getSingleCategory(String id) {
 		
 		return mongoTemplate.findById(id, Category.class, COLLECTION_NAME);
 	}
 	
+	//Get Single Category with its name
+	@Override
+	public Category getSingleCategoryByName(String name){
+		Query query = new Query();
+		query.addCriteria(Criteria.where("name").is(name));
+		return mongoTemplate.findOne(query, Category.class);
+	}
+	
+	//Find Single category's parent tree
+	public List<String> getAllParents(String parentName){
+		
+		Query query = new Query();
+		query.addCriteria(Criteria.where("name").is(parentName));
+		ArrayList<String> catParents = new ArrayList<String>();
+		
+		Category category = mongoTemplate.findOne(query, Category.class);
+		// TODO: category parent tree for breadcrumb
+		for(String par : category.getAncestors()){
+			if(category.getParent().equals(par)){
+				catParents.add(category.getParent());
+				category.setName(par);
+			}
+		}
+		
+		return catParents;
+		
+	}
+	
+	//Save Category
 	@Override
 	public void saveCategory(Category category) {
 		
