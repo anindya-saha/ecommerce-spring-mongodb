14f257701fa8f8790a4db7ff9c2a1338782e4f16
İlker Korkut <ilkerkorkut@outlook.com>
2013-09-11 01:10:02 +0300

    Configured product category tree

--- src/main/java/com/ecommerce/controller/ProductController.java
+++ src/main/java/com/ecommerce/controller/ProductController.java
@@ -1,8 +1,6 @@
 package com.ecommerce.controller;
 
-import java.util.Date;
-import java.util.List;
-import java.util.Map;
+import java.util.HashMap;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
@@ -10,15 +8,16 @@ import org.apache.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
-import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
 
 import com.ecommerce.constant.ConfirmationStatus;
+import com.ecommerce.model.Category;
 import com.ecommerce.model.Product;
 import com.ecommerce.model.ProductComment;
+import com.ecommerce.service.CategoryService;
 import com.ecommerce.service.ProductService;
 
 @Controller
@@ -27,26 +26,31 @@ public class ProductController {
 
 	@Autowired
 	private ProductService productService;
+	
+	@Autowired
+	private CategoryService categoryService;
 
 	@RequestMapping(value = "/product", method = RequestMethod.GET)
 	public ModelAndView showProduct(
 			@RequestParam(value = "productid", required = false) String productId,
 			ModelMap model, HttpServletRequest request,
 			HttpServletResponse response) {
-		ModelAndView modelAndView = new ModelAndView("productDetails");
+		ModelAndView mav = new ModelAndView("productDetails");
 
 		logger.debug("Product Controller logger");
+		
 		Product singleProduct = productService.singleProduct(productId);
-		// List<ProductComments> productComments =
-		// this.productService.productComments(productId,
-		// ConfirmationStatus.CONFIRMED.getConfirmation());
-		// modelAndView.addObject("urunler", products);
-		model.addAttribute("product", singleProduct);
-		// modelAndView.addObject("comments", productComments);
-		// model.addAttribute("urunler",products);
-		// model.addAttribute("singleProduct",singleProduct);
+		
+		Category category = categoryService.getCategoryByName(singleProduct.getCategory().getParent());
+		
+		HashMap<String,String> catMap = categoryService.getCategoryTree(singleProduct.getCategory().getParent());
 
-		return modelAndView;
+		mav.addObject("prodCategory",singleProduct.getCategory().getName());
+		mav.addObject("categoryList",catMap);
+		
+		model.addAttribute("product", singleProduct);
+
+		return mav;
 	}
 
 
