ef3d65c69c51adae0aecd4d9dac667f4f9e1e26e
İlker Korkut <ilkerkorkut@outlook.com>
2013-09-14 02:48:21 +0300

    update

--- src/main/java/com/ecommerce/controller/HomepageController.java
+++ src/main/java/com/ecommerce/controller/HomepageController.java
@@ -3,30 +3,36 @@ package com.ecommerce.controller;
 import java.util.List;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.SessionAttributes;
 import org.springframework.web.servlet.ModelAndView;
 
 import com.ecommerce.model.Product;
 import com.ecommerce.service.ProductService;
 
 @Controller
+@RequestMapping(value = "/homepage")
+@SessionAttributes({"user"})
 public class HomepageController {
 	@Autowired
 	private ProductService productService;
 
-	@RequestMapping(value = "/homepage", method = RequestMethod.GET)
+	@RequestMapping(method = RequestMethod.GET)
 	public ModelAndView homePage(HttpServletRequest request,
 			HttpServletResponse response) {
-		ModelAndView modelAndView = new ModelAndView("homepage");
+		ModelAndView mav = new ModelAndView("homepage");
 
 		List<Product> products = productService.listOrderedProducts("productAddedDate","DESC",4);
+				
+		mav.addObject("products", products);
 
-		modelAndView.addObject("products", products);
-
-		return modelAndView;
+		return mav;
 	}
 
 	@RequestMapping(value = { "/register" }, method = { org.springframework.web.bind.annotation.RequestMethod.GET })
