748407d8c4208e0f9446f9552a9f3cc193a338e0
İlker Korkut <ilkerkorkut@outlook.com>
2013-09-14 02:48:46 +0300

    user login

---
+++ src/main/java/com/ecommerce/controller/LoginController.java
@@ -0,0 +1,56 @@
+package com.ecommerce.controller;
+
+import org.apache.log4j.Logger;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.SessionAttributes;
+import org.springframework.web.bind.support.SessionStatus;
+import org.springframework.web.servlet.ModelAndView;
+
+import com.ecommerce.model.User;
+import com.ecommerce.service.UserService;
+
+@Controller
+@RequestMapping(value="/login")
+@SessionAttributes({"user"})
+public class LoginController {
+	
+	protected static Logger logger = Logger.getLogger("controller");
+	
+	@Autowired
+	private UserService userService;
+	
+	@RequestMapping(method = RequestMethod.GET)
+	public ModelAndView loginPage(){
+		ModelAndView mav = new ModelAndView();
+		mav.setViewName("login");
+		return mav;
+	}
+	
+	@RequestMapping(value = "/login-process", method = RequestMethod.POST)
+	public String loginProcess(
+			@RequestParam(value="name", required = false) String name,
+			@RequestParam(value="password", required = false) String password,
+			Model model) throws Exception{
+		
+		boolean login = userService.loginUser(name,password);
+		if(login){
+			User user = userService.getUserByName(name);
+			model.addAttribute(user);
+		}
+		
+		return "redirect:/homepage";
+	}
+	
+	@RequestMapping(value="/logout")
+	public String logout(SessionStatus status){
+		
+		status.setComplete();
+		return "redirect:/homepage";
+	}
+	
+}
