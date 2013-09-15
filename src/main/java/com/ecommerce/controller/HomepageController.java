72eb162270bd6d9d46a4e08ec8b314a4c4143abf
İlker Korkut <ilkerkorkut@outlook.com>
2013-09-15 03:23:12 +0300

    inject

--- src/main/java/com/ecommerce/controller/HomepageController.java
+++ src/main/java/com/ecommerce/controller/HomepageController.java
@@ -1,13 +1,12 @@
 package com.ecommerce.controller;
 
 import java.util.List;
+
+import javax.inject.Inject;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import javax.servlet.http.HttpSession;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.SessionAttributes;
@@ -20,7 +19,8 @@ import com.ecommerce.service.ProductService;
 @RequestMapping(value = "/homepage")
 @SessionAttributes({"user"})
 public class HomepageController {
-	@Autowired
+	
+	@Inject
 	private ProductService productService;
 
 	@RequestMapping(method = RequestMethod.GET)
