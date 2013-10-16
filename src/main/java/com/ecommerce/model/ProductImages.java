0c39552bbb1811c6d87ee4782f47e04150fe68cb
İlker Korkut <ilkerkorkut@outlook.com>
2013-10-16 03:07:00 +0300

    .

--- src/main/java/com/ecommerce/model/ProductImages.java
+++ src/main/java/com/ecommerce/model/ProductImages.java
@@ -4,6 +4,22 @@ public class ProductImages {
 	
 	private String id;
 	
-	private String[] imageUrl;
+	private String[] images;
+	
+	public String getId(){
+		return this.id;
+	}
+	
+	public void setId(String id){
+		this.id = id;
+	}
+	
+	public String[] getImages(){
+		return this.images;
+	}
+	
+	public void setImages(String[] images){
+		this.images = images;
+	}
 	
 }
