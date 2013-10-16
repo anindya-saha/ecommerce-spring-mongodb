86aaca347280ab4aada4e2b1b91dc19d8a5f9def
İlker Korkut <ilkerkorkut@outlook.com>
2013-10-16 03:07:23 +0300

    add to cart

--- src/main/java/com/ecommerce/controller/ProductController.java
+++ src/main/java/com/ecommerce/controller/ProductController.java
@@ -25,6 +25,7 @@ import org.springframework.web.bind.annotation.SessionAttributes;
 import org.springframework.web.servlet.ModelAndView;
 
 import com.ecommerce.constant.ConfirmationStatus;
+import com.ecommerce.model.CartItem;
 import com.ecommerce.model.Category;
 import com.ecommerce.model.Product;
 import com.ecommerce.model.ProductComment;
@@ -97,30 +98,68 @@ public class ProductController {
 			HttpSession session){
 		//TODO: add count for one product item
 		boolean checkContain = false;
-		Product product = productService.singleProduct(productId);
+		double total = 0;
+		//Product product = productService.singleProduct(productId);
+		CartItem cartItem = new CartItem();
+		cartItem.setProduct(productService.singleProduct(productId));
+		cartItem.setCount(1);
 		if(session.getAttribute("cart") == null || session.getAttribute("cart") == ""){
-			List<Product> productList = new ArrayList<Product>();
-			productList.add(product);
-			session.setAttribute("cart", productList);
+			List<CartItem> cartItemList = new ArrayList<CartItem>();
+			cartItemList.add(cartItem);
+			session.setAttribute("cart", cartItemList);
+			total = cartItem.getProduct().getPrice();
+			session.setAttribute("cartTotal",total);
 		}
 		else{
-			List<Product> productList = new ArrayList<Product>();
-			productList.addAll((Collection<? extends Product>) session.getAttribute("cart"));
-			for(Product p : productList){
-				if(p.getId().equals(product.getId())){
+			List<CartItem> cartItemList = new ArrayList<CartItem>();
+			cartItemList.addAll((Collection<? extends CartItem>) session.getAttribute("cart"));
+			for(CartItem c : cartItemList){
+				if(c.getProduct().getId().equals(cartItem.getProduct().getId())){
 					checkContain = true;
+					cartItem.setCount(c.getCount()+1);
+					cartItemList.remove(c);
+					cartItemList.add(cartItem);
+					session.setAttribute("cart", cartItemList);
 				}
 			}
 			if(checkContain){
-				return "Product is already in your cart";
+				
+				for(CartItem c : cartItemList){
+					total += c.getProduct().getPrice();
+				}
+				session.setAttribute("cartTotal", total);
+				return "Product is already in your cart, so product count increased by one";
 			}
 			else{
-				productList.add(product);
-				session.setAttribute("cart", productList);
+				cartItemList.add(cartItem);
+				session.setAttribute("cart", cartItemList);
+				for(CartItem c : cartItemList){
+					total += c.getProduct().getPrice();
+				}
+				session.setAttribute("cartTotal", total);
 			}
 		}
 		
 		return "added product to cart";
+	}
+	
+	@SuppressWarnings("unchecked")
+	@ResponseBody
+	@RequestMapping(value="/remove-from-cart", method = RequestMethod.POST)
+	public String deleteProductFromCart(
+			@RequestParam(value = "productId" ,required = true) String productId,
+			HttpSession session){
+		
+		Product product = productService.singleProduct(productId);
+		
+		
+		List<Product> productList = new ArrayList<Product>();
+		productList.addAll((Collection<? extends Product>) session.getAttribute("cart"));
+		
+		productList.remove(product);
+		session.setAttribute("cart", productList);
+		
+		return "Product removed from the cart";
 	}
 
 }
---
+++ src/main/java/com/ecommerce/model/CartItem.java
@@ -0,0 +1,22 @@
+package com.ecommerce.model;
+
+public class CartItem {
+	
+	private Product product;
+	
+	private int count;
+	
+	public Product getProduct(){
+		return this.product;
+	}
+	public void setProduct(Product product){
+		this.product = product;
+	}
+	
+	public int getCount(){
+		return this.count;
+	}
+	public void setCount(int count){
+		this.count = count;
+	}
+}
