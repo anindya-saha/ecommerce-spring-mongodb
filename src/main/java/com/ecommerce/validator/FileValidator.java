28c7ceee62208b066be119f00738671fb21c0113
İlker Korkut <ilkerkorkut@outlook.com>
2013-11-21 00:41:23 +0200

    removed unusused lines

--- src/main/java/com/ecommerce/validator/FileValidator.java
+++ src/main/java/com/ecommerce/validator/FileValidator.java
@@ -3,8 +3,6 @@ package com.ecommerce.validator;
 import org.springframework.validation.Errors;
 import org.springframework.validation.Validator;
 
-import com.ecommerce.model.UploadedFile;
-
 public class FileValidator implements Validator{
 
 	@Override
@@ -15,11 +13,11 @@ public class FileValidator implements Validator{
 	@Override
 	public void validate(Object uploadedFile, Errors errors) {
 
-		UploadedFile file = (UploadedFile) uploadedFile;
-		
-		if(file.getFile().getSize() == 0){
-			errors.rejectValue("file", "Please select a file!");
-		}
+//		UploadedFile file = (UploadedFile) uploadedFile;
+//		
+//		if(file.getFile().getSize() == 0){
+//			errors.rejectValue("file", "Please select a file!");
+//		}
 		
 	}
 
