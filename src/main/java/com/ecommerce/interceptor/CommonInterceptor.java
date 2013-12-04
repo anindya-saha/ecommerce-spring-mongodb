d1375476a57c602997cee749ca7c1f483bb88c38
İlker Korkut <ilkerkorkut@outlook.com>
2013-12-05 01:13:29 +0200

    nginx staticServer

--- src/main/java/com/ecommerce/interceptor/CommonInterceptor.java
+++ src/main/java/com/ecommerce/interceptor/CommonInterceptor.java
@@ -1,51 +1,22 @@
 package com.ecommerce.interceptor;
 
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
-
-import javax.inject.Inject;
-import javax.servlet.http.Cookie;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import javax.servlet.http.HttpSession;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.servlet.ModelAndView;
 import org.springframework.web.servlet.HandlerInterceptor;
 
-import com.ecommerce.model.Category;
-import com.ecommerce.model.User;
-import com.ecommerce.service.CategoryService;
-import com.ecommerce.service.UserService;
+import com.ecommerce.constant.StaticServer;
 
 public class CommonInterceptor implements HandlerInterceptor{
-	
-	@Inject
-	private CategoryService categoryService;
-	@Inject
-	private UserService userService;
+
 
 	@Override
 	public void postHandle(HttpServletRequest request,HttpServletResponse response, Object handler,ModelAndView mav) throws Exception{
-//		System.out.println("posthandle");
-//		HttpSession session = request.getSession();
-		List<Category> categoryList = categoryService.getCategories();
-		mav.addObject("catList",categoryList);
+		System.out.println("posthandle");
 		
-//		Cookie[] cookies = request.getCookies();
-//		if(cookies != null){
-//			for(Cookie cookie : cookies){
-//				if(cookie.getValue().equals("userId")){
-//					User user = userService.getUserById(cookie.getValue());
-//					//model.addAttribute(user);
-//					session.setAttribute("user", user);
-//				}
-//				else{
-//					//login = userService.loginUser(name,password);
-//				}
-//			}
-//		}
+		//StaticServer (nginx server)
+		request.setAttribute("staticServer", StaticServer.PROTOCOL.getConfiguration()+StaticServer.DOMAIN.getConfiguration());
 		
 	}
 
