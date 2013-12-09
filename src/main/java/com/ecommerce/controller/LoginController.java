e88b316871b5b9c1ddabf8d365b005dfdde0887c
İlker Korkut <ilkerkorkut@outlook.com>
2013-12-09 02:54:19 +0200

    login controller changed for encrypted password

--- src/main/java/com/ecommerce/controller/LoginController.java
+++ src/main/java/com/ecommerce/controller/LoginController.java
@@ -2,28 +2,26 @@ package com.ecommerce.controller;
 
 import javax.inject.Inject;
 import javax.servlet.http.Cookie;
-import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 
 import org.apache.log4j.Logger;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.SessionAttributes;
 import org.springframework.web.bind.support.SessionStatus;
 import org.springframework.web.servlet.ModelAndView;
 
 import com.ecommerce.model.User;
+import com.ecommerce.security.PasswordEncryption;
 import com.ecommerce.service.UserService;
 
 @Controller
 @RequestMapping(value="/user")
 public class LoginController {
 	
-	protected static Logger logger = Logger.getLogger("controller");
+	protected static Logger logger = Logger.getLogger(LoginController.class);
 	
 	@Inject
 	private UserService userService;
@@ -43,7 +41,9 @@ public class LoginController {
 			HttpServletResponse response,
 			HttpSession session) throws Exception{
 		
-		boolean login = userService.loginUser(email,password);
+		String encPass = PasswordEncryption.encrypt(password);
+		
+		boolean login = userService.loginUser(email,encPass);
 		
 		if(login){
 			User user = userService.getUserByEmail(email);
